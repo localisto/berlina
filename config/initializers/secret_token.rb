21365739b59948c929bcaac0f53b2da8532d5f5c
Michael Haddad <michael@ludditetechnology.com>
2012-09-04 00:32:46 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evergreen::Application.config.secret_token = '88e78676326950c05977b1bedd4611c7fc38557187579c372226fda2ef9c0c9463430e9bbf831458cfc922168131fcd811425789fa845ee07cf94a167f18835c'
