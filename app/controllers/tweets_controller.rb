4293dd4945b924631a6db1d13513383ee2c54032
Michael Haddad <mrhaddad@Michaels-MacBook-Air.local>
2012-09-05 14:51:01 -0300

    Remove n+1 warning for tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
   def index
-    render json: Tweet.for_hashtag(params[:hashtag]).order("created_at desc").first(10)
+    render json: Tweet.for_hashtag(params[:hashtag]).includes(:twitter_user).order("created_at desc").first(10)
   end
 end
